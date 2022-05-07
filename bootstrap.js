f7e827b51cfe2d8e9c19af5ccc0ba4de3613c40d
dingdongzhao <ddzyan@163.com>
2022-05-08 00:29:00 +0800

    ci(修改启动指令): 🐎

--- bootstrap.js
+++ bootstrap.js
@@ -1,9 +1,2 @@
-const WebFramework = require('@midwayjs/web').Framework;
-
-const web = new WebFramework().configure({
-  port: 7001,
-  hostname: '0.0.0.0',
-});
-
 const { Bootstrap } = require('@midwayjs/bootstrap');
-Bootstrap.load(web).run();
+Bootstrap.run();
