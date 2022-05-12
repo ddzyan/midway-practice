1f3618e2ee511dbad4ea967cafc2de16322d7b0b
dingdongzhao <ddzyan@163.com>
2022-05-12 22:56:08 +0800

    feat(添加 jwt加解密和redis锁): ✨

--- src/app/comm/redis.ts
+++ src/app/comm/redis.ts
@@ -5,42 +5,47 @@ import { RedisService } from '@midwayjs/redis';
 @Scope(ScopeEnum.Singleton) // 单例
 class RedisUtils {
   @Inject()
-  redisService: RedisService;
+  private redis: RedisService;
 
-  async getLock(key: string, expireTime: number) {
-    const lock = await this.redisService.get(key);
+  public async checkLock(key: string) {
+    const result = await this.redis.get(key);
+    return !!result;
+  }
+
+  public async getLock(key: string, expireTime: number) {
+    const lock = await this.redis.get(key);
     if (lock) {
       return false;
     }
 
-    await this.redisService.set(key, 'true', 'EX', expireTime);
+    await this.redis.set(key, 1, 'EX', expireTime);
     return true;
   }
 
-  async unLock(key) {
-    const lock = await this.redisService.get(key);
+  public async unLock(key) {
+    const lock = await this.redis.get(key);
     if (!lock) {
       return true;
     }
 
-    const res = await this.redisService.del(key);
+    const res = await this.redis.del(key);
     return res;
   }
 
-  async setValue(key: string, value: string, expireTime?: number) {
+  public async setValue(key: string, value: string, expireTime?: number) {
     if (!expireTime) {
-      await this.redisService.set(key, value);
+      await this.redis.set(key, value);
     } else {
-      await this.redisService.set(key, value, 'EX', expireTime);
+      await this.redis.set(key, value, 'EX', expireTime);
     }
   }
 
-  async getString(key: string) {
+  public async getString(key: string) {
     if (!key) {
       return null;
     }
 
-    return await this.redisService.get(key);
+    return await this.redis.get(key);
   }
 }
 
