cfd2e4449472e304267a0d1023ffb32422fc8eb3
dingdongzhao <ddzyan@163.com>
2022-07-25 10:02:27 +0800

    refactor: 🦄 优化sequelize配置

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/test/fixtures'],
+  coveragePathIgnorePatterns: ['<rootDir>/test/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  bail: true,
+};
--- src/core/baseEntity.ts
+++ src/core/baseEntity.ts
@@ -1,24 +1,12 @@
-import { Column, DataType, Model } from 'sequelize-typescript';
+import { Model, UpdatedAt, CreatedAt, DeletedAt } from 'sequelize-typescript';
 
 export class BaseEntity extends Model {
-  @Column({
-    type: DataType.DATE,
-    field: 'created_at',
-    comment: '创建时间',
-  })
+  @CreatedAt
   createdAt: Date;
 
-  @Column({
-    type: DataType.DATE,
-    field: 'updated_at',
-    comment: '更新时间',
-  })
+  @UpdatedAt
   updatedAt: Date;
 
-  @Column({
-    type: DataType.DATE,
-    field: 'deleted_at',
-    comment: '删除时间',
-  })
+  @DeletedAt
   deletedAt: Date;
 }
